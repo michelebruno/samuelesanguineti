30551231db1a8972e43006573ec36011e515d4d8
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-04-14 00:17:59 +0200

    sanbenedetto.js and gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,14 @@ module.exports = {
     plugins: [
         'gatsby-plugin-sass',
         'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: `${__dirname}/src/images`,
+            },
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
     ]
 }
--- src/pages/sanbenedetto.js
+++ src/pages/sanbenedetto.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import {graphql} from 'gatsby';
+import GatsbyImage from 'gatsby-image';
 
-export default function SanBenedetto() {
+export default function SanBenedetto({data: {images}}) {
   return <Layout header={'Thé San Benedetto'}>
-    <p >
+    <p>
       In the San Benedetto project, is enclosed all the work behind the creation
       of tea. Through illustrations created specifically for each taste, trying
       to create a macro environment, growth, which is divided into two branches,
@@ -19,5 +21,44 @@ export default function SanBenedetto() {
     <p><b>Role:</b> Creation of illustrations, branding, creation of digital
       content.</p>
     <p><b>NABA 2018</b></p>
+    <div className="row no-gutters"  >
+      <div className="col-12 col-md-6 p-1"><GatsbyImage
+          fluid={images.nodes[6].childImageSharp.fluid}/>
+      </div>
+      <div className="col-12 col-md-6 p-1"><GatsbyImage
+          fluid={images.nodes[3].childImageSharp.fluid}/>
+      </div>
+      <div className="col-12 col-md-6 p-1"><GatsbyImage
+          fluid={images.nodes[4].childImageSharp.fluid}/>
+      </div>
+      <div className="col-12 col-md-6 p-1"><GatsbyImage
+          fluid={images.nodes[5].childImageSharp.fluid}/>
+      </div>
+      <div className="w-100"></div>
+      <div className="col-4">
+        <GatsbyImage
+            fluid={images.nodes[0].childImageSharp.fluid}/>
+      </div>
+      <div className="col-4"><GatsbyImage
+          fluid={images.nodes[1].childImageSharp.fluid}/>
+      </div>
+      <div className="col-4"><GatsbyImage
+          fluid={images.nodes[2].childImageSharp.fluid}/>
+      </div>
+    </div>
   </Layout>;
 }
+export const query = graphql`{
+    images: allFile(filter: {relativePath: {regex: "/sb_(.*)/i"}}) { 
+      nodes {
+        id
+        relativePath
+        childImageSharp {
+          fluid(maxWidth: 1200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      } 
+    }
+  }
+`;
