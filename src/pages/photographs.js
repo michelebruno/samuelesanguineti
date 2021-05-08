e11608492ef5f404e0cf69301abcef5d7ca5615d
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-05-09 01:00:16 +0200

    Photographs

--- src/pages/photographs.js
+++ src/pages/photographs.js
@@ -8,26 +8,6 @@ export default function SanBenedetto({data: {images}}) {
 
   return <Layout header={'Personal photo'}>
     <Seo title={'Personal photo'}/>
-    <p>The Archetypical Innovation in Sportswear workshop focuses on the
-      redesign of a vintage garment, its actualization in terms of design,
-      material technologies and production processes. The whole experience
-      considered each part of the design process, starting from the marketing
-      strategy, passing through the design and engineering of the product,
-      ending with a deep reflection regarding the communication of the
-      sustainable features at different levels. The outcome had considered the
-      garment development, tackling all the research/ production/ communication
-      steps, with a strong focus on sustainability, innovative technologies and
-      production feasibility.
-    </p>
-    <p><b>Role:</b> creation of communication content, branding, creation of a
-      service, creation of customer journey, creation of app and web interface,
-      study of an advertising campaign, study of trends, study of
-      sustainability.
-    </p>
-    <p><b>Group members:</b> Anna Baroni, Veronica Busignani, Paola Mazza,
-      Samuele Sanguineti, Matilde Voltolini
-    </p>
-    <p><b>Politecnico di Milano 2020</b></p>
     <div className="row project-gallery">
       <div
           className="col-12 offset-lg-2 col-lg-3 d-flex flex-column justify-content-between">
